74f2602b8ea894c63bb1a066cfb86622b3e3f279
aserero <anthony@taliacap.com>
2026-03-31 09:38:35 +0200

    modif name

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
+document.documentElement.lang = 'fr'
+document.documentElement.setAttribute('translate', 'no')
+document.title = 'NAKY'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
 )
